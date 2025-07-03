de33d787936fce468d01a93d60147fe8cc55fa13
almir1904 <almir1904@users.noreply.github.com>
2025-07-03 23:27:09 +0200

    Add public hostname option

--- gimuserver/system/ServerConfig.cpp
+++ gimuserver/system/ServerConfig.cpp
@@ -2,8 +2,10 @@
 
 void ServerConfig::ParseFromJson(const Json::Value& v)
 {
-	Wallpaper = v["wallpaper_banner"].asString();
-	GameVersion = v["game_version"].asInt();
-	NoticeUrl = v["notice_url"].asString();
+        Wallpaper = v["wallpaper_banner"].asString();
+        GameVersion = v["game_version"].asInt();
+        NoticeUrl = v["notice_url"].asString();
+        if (v.isMember("public_hostname"))
+                PublicHostname = v["public_hostname"].asString();
 }
 
