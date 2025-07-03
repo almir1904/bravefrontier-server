7c8b5cc0e2b18d862d4bb67f7d008d54b397f582
almir1904 <almir1904@users.noreply.github.com>
2025-07-03 19:23:43 +0200

    Update Docker setup for Debian

--- gimuserver/gme/handlers/UserInfoHandler.cpp
+++ gimuserver/gme/handlers/UserInfoHandler.cpp
@@ -190,7 +190,7 @@ void Handler::UserInfoHandler::Handle(UserInfo& user, DrogonCallback cb, const J
         [this, &user, cb](const drogon::orm::Result& result) {
         if (result.size() > 0) {
             int col = 0;
-            auto& sql = result[0];
+            auto sql = result[0];
             user.teamInfo.UserID = user.info.userID;
             user.teamInfo.Level = sql[col++].as<uint32_t>();
             user.teamInfo.Exp = sql[col++].as<int64_t>();
